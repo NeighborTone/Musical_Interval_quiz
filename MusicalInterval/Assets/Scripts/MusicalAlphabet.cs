0451e94b95a65c34b5747e6537e4d1841e01019b
tonarinohito <rsp44316@gmail.com>
2023-03-09 23:23:38 +0900

    init

---
+++ MusicalInterval/Assets/Scripts/MusicalAlphabet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct MusicalAlphabet
+{
+    public enum Alphabet
+    {
+        C = 0,
+        C_s = 1,  //D_b
+        D = 2,
+        D_s = 3,  //E_b
+        E = 4,
+        F = 5,
+        F_s = 6,  //G_b
+        G = 7,
+        G_s = 8,  //A_b
+        A = 9,
+        A_s = 10, //B_b
+        B = 11,
+        MAX
+    }
+}
